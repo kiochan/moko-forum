9a4366b1083852bff65a94c7c02c1e017db8ba67
kiochan <sunxingchen@live.com>
2023-04-14 15:16:31 +0200

    pm2 脚本

---
+++ pm2.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "moko-forum",
+      script: "npm",
+      args: "deploy",
+      watch: true,
+      // Delay between restart
+      watch_delay: 10000,
+      ignore_watch: ["node_modules", ".next", "code_examples", "data"],
+    },
+  ],
+};
